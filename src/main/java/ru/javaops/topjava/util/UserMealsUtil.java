fa2b2888c62762d6bce84c221d97aff24387ed45
Igor K <ikopyshev.dev@gmail.com>
2025-09-21 06:39:37 +0400

    [HW0] Refactoring

--- src/main/java/ru/javaops/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javaops/topjava/util/UserMealsUtil.java
@@ -13,21 +13,27 @@ import java.util.HashMap;
 import java.util.List;
 
 public class UserMealsUtil {
+
+    private static final int CALORIES_PER_DAY = 2000;
+    private static final LocalTime startTime = LocalTime.of(7, 0);
+    private static final LocalTime endTime = LocalTime.of(12, 0);
+
+    private static final List<UserMeal> meals = Arrays.asList(
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    );
+
     public static void main(String[] args) {
-        List<UserMeal> meals = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 13, 0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 30, 20, 0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2025, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-        );
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, startTime, endTime, CALORIES_PER_DAY);
+        mealsTo.forEach(System.out::println); // NOSONAR
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsTo.forEach(System.out::println); // NOSONAR logging will appear later
-
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000)); // NOSONAR logging will appear later
+        mealsTo = filteredByStreams(meals, startTime, endTime, CALORIES_PER_DAY);
+        mealsTo.forEach(System.out::println); // NOSONAR
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
