845c5edfce296a2a13bedb8eeccd541e8d2f5f8f
Igor K <ikopyshev.dev@gmail.com>
2025-09-21 22:28:03 +0400

    [Lesson 1] Logback configuration

--- src/main/java/ru/javaops/topjava/web/UserServlet.java
+++ src/main/java/ru/javaops/topjava/web/UserServlet.java
@@ -3,12 +3,18 @@ package ru.javaops.topjava.web;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        log.info("Redirecting to {}", req.getRequestURI());
         resp.sendRedirect("users.jsp"); // NOSONAR will be replaced by controllers
     }
 }
